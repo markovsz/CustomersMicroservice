1fe1c17109260cfbc47d32a2b95780349b30c059
Stas <markovszzzz@gmail.com>
2022-06-17 14:49:01 +0300

    Services dependencies was added

---
+++ Messager.Customers.API/ServiceExtentions.cs
@@ -0,0 +1,39 @@
+﻿using Messager.Customers.Domain.Interfaces.Repositories.AdministratorSubsystem;
+using Messager.Customers.Domain.Interfaces.Repositories.CustomerSubsystem;
+using Messager.Customers.Domain.Interfaces.Repositories.IGuestSubsystem;
+using Messager.Customers.Infrastructure.Data;
+using Messager.Customers.Infrastructure.Data.Repositories.AdministratorSubsystem;
+using Messager.Customers.Infrastructure.Data.Repositories.CustomerSubsystem;
+using Messager.Customers.Infrastructure.Data.Repositories.GuestSubsystem;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Messager.Customers.API
+{
+    public static class ServiceExtentions
+    {
+        public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+                b => b.MigrationsAssembly("Messager_Customers")));
+        }
+
+        public static void ConfigureRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IAdministratorManager, AdministratorManager>();
+            services.AddScoped<ICustomersRepositoryForAdministrator, CustomersRepositoryForAdministrator>();
+            services.AddScoped<ICustomerManager, CustomerManager>();
+            services.AddScoped<ICustomersRepositoryForCustomer, CustomersRepositoryForCustomer>();
+            services.AddScoped<IGuestManager, GuestManager>();
+            services.AddScoped<ICustomersRepositoryForGuest, CustomersRepositoryForGuest>();
+        }
+
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAdministratorManager, AdministratorManager>();
+            services.AddScoped<ICustomerManager, CustomerManager>();
+            services.AddScoped<IGuestManager, GuestManager>();
+        }
+    }
+}
--- Messager.Customers.API/Startup.cs
+++ Messager.Customers.API/Startup.cs
@@ -1,6 +1,8 @@
+using Messager.Customers.Infrastructure.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +14,21 @@ namespace Messager.Customers.API
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureDbContext(_configuration);
+            services.ConfigureRepositories();
+            services.AddAutoMapper(typeof(MappingProfile));
+            services.ConfigureServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
