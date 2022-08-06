78a147794f6443769572bca13212e005534c5aa9
Stas <markovszzzz@gmail.com>
2022-08-06 22:36:44 +0300

    repository manager was added

---
+++ Messager.Customers.Infrastructure.Data/RepositoryManager.cs
@@ -0,0 +1,31 @@
+﻿using Messager.Customers.Domain.Interfaces;
+using Messager.Customers.Domain.Interfaces.Repositories;
+using Messager.Customers.Infrastructure.Data.Repositories;
+using System.Threading.Tasks;
+
+namespace Messager.Customers.Infrastructure.Data
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private RepositoryContext _context;
+        private ICustomersRepository _customersRepository;
+
+        public RepositoryManager(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public ICustomersRepository Customers
+        {
+            get
+            {
+                if (_customersRepository is null)
+                    _customersRepository = new CustomersRepository(_context);
+                return _customersRepository;
+            }
+        }
+
+        public async Task SaveAsync() => 
+            await _context.SaveChangesAsync();
+    }
+}
