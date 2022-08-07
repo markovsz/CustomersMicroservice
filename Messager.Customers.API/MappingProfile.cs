73f9356ead619bfeaa18a00b041b901aa55c3370
Stas <markovszzzz@gmail.com>
2022-08-07 22:38:14 +0300

    mapping profile was added

---
+++ Messager.Customers.API/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Messager.Customers.Application.Services.DataTransferObjects;
+using Messager.Customers.Domain.Core.Models;
+
+namespace Messager.Customers.API
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CustomerForCreateDto, Customer>();
+            CreateMap<Customer, CustomerForReadPublicInfoDto>();
+            CreateMap<Customer, CustomerForReadPrivateInfoDto>();
+            CreateMap<Customer, CustomerForReadMinimizedDto>();
+            CreateMap<CustomerForUpdateDto, Customer>();
+        }
+    }
+}
