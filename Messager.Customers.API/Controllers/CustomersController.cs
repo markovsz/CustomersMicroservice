6441a21713518b7b7bf958e9188a9ca5cafea125
Stas <markovszzzz@gmail.com>
2022-08-07 22:37:50 +0300

    controllers were added

---
+++ Messager.Customers.API/Controllers/CustomersController.cs
@@ -0,0 +1,80 @@
+﻿using Messager.Customers.API.Filters;
+using Messager.Customers.Application.Services.DataTransferObjects;
+using Messager.Customers.Application.Services.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Messager.Customers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        
+        public ICustomersService _customersService;
+
+        public CustomersController(ICustomersService customersService)
+        {
+            _customersService = customersService;
+        }
+
+
+        [Authorize(Roles = "Administrator,Customer")]
+        [HttpGet("all")]
+        public async Task<IActionResult> GetCustomersAsync()//TODO: pagination
+        {
+            var customers = await _customersService.GetCustomersAsync();
+            return Ok(customers);
+        }
+
+
+        [Authorize(Roles = "Administrator,Customer")]
+        [HttpPost("min")]
+        public IActionResult GetMinimizedCustomersAsync([FromBody] IEnumerable<Guid> userIds)
+        {
+            var customers = _customersService.GetMinimizedCustomersInfoByUserIds(userIds);
+            return Ok(customers);
+        }
+
+
+        [Authorize(Roles = "Administrator,Customer")]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomerInfoByUserIdAsync(Guid userId)
+        {
+            var customer = await _customersService.GetCustomerByUserIdAsync(userId, false);
+            return Ok(customer);
+        }
+
+
+        [Authorize(Roles = "Administrator,Customer")]
+        [ServiceFilter(typeof(ExtractUserIdFilter))]
+        [HttpGet("", Name = "GetCustomer")]
+        public async Task<IActionResult> GetCustomerInfoAsync(Guid userId)
+        {
+            var customer = await _customersService.GetCustomerByUserIdAsync(userId, false);
+            return Ok(customer);
+        }
+
+
+        [Authorize(Roles = "Administrator,Customer")]
+        [ServiceFilter(typeof(ExtractUserIdFilter))]
+        [HttpPut("")]
+        public async Task<IActionResult> UpdateCustomerAsync(Guid userId, CustomerForUpdateDto customerDto)
+        {
+            await _customersService.UpdateCustomerAsync(userId, customerDto);
+            return NoContent();
+        }
+
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPut("{userId}")]
+        public async Task<IActionResult> UpdateCustomerByUserIdAsync(Guid userId, CustomerForUpdateDto customerDto)
+        {
+            await _customersService.UpdateCustomerAsync(userId, customerDto);
+            return NoContent();
+        }
+    }
+}
---
+++ Messager.Customers.API/Controllers/RelatedEntitiesController.cs
@@ -0,0 +1,43 @@
+﻿using Messager.Customers.API.Filters;
+using Messager.Customers.Application.Services.DataTransferObjects;
+using Messager.Customers.Application.Services.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messager.Customers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RelatedEntitiesController : ControllerBase
+    {
+        public ICustomersService _customersService;
+
+        public RelatedEntitiesController(ICustomersService customersService)
+        {
+            _customersService = customersService;
+        }
+
+        [Authorize(Roles = "Administrator,Customer")]/*!*/
+        [ServiceFilter(typeof(ExtractUserIdFilter))]
+        [HttpPost("")]
+        public async Task<IActionResult> CreateCustomerAsync(Guid userId, CustomerForCreateDto customerDto)
+        {
+            var createdUser = await _customersService.CreateCustomerAsync(userId, customerDto);
+            return CreatedAtRoute("GetCustomer", createdUser);
+        }
+
+        [Authorize(Roles = "Administrator,Customer")]
+        [ServiceFilter(typeof(ExtractUserIdFilter))]
+        [HttpDelete("")]
+        public async Task<IActionResult> DeleteRelatedToUserEntitiesAsync(Guid userId)
+        {
+            await _customersService.DeleteCustomerByUserIdAsync(userId);
+            return NoContent();
+        }
+    }
+}
