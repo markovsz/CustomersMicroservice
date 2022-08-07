8ccb8da90e822f3ee8c8b52d6797e570e0dc7a4a
Stas <markovszzzz@gmail.com>
2022-08-07 22:36:53 +0300

    filters were added

---
+++ Messager.Customers.API/Filters/ExtractRoleFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Messager.Customers.API.Filters
+{
+    public class ExtractRoleFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var role = context.HttpContext.User.Claims
+                .Where(c => c.Type.Equals(ClaimTypes.Role))
+                .Select(c => c.Value)
+                .FirstOrDefault();
+            context.ActionArguments.Add("userRole", role);
+        }
+    }
+}
---
+++ Messager.Customers.API/Filters/ExtractUserIdFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Messager.Customers.API.Filters
+{
+    public class ExtractUserIdFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var action = context.RouteData.Values["action"];
+            var controller = context.RouteData.Values["controller"];
+            var userIdClaim = context.HttpContext.User.Claims
+                .Where(c => c.Type.Equals(ClaimTypes.NameIdentifier))
+                .FirstOrDefault();
+            Guid userId;
+            Guid.TryParse(userIdClaim.Value, out userId);
+            context.ActionArguments.Add("userId", userId); /*!*/
+        }
+    }
+}
