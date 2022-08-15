7373620fc9b7e8991a3ef4f35f1692f97887b0cd
Stas <markovszzzz@gmail.com>
2022-08-15 16:25:59 +0300

    dto's were updated (image icon src was added)

--- Messager.Customers.Application.Services/DataTransferObjects/CustomerForReadMinimizedDto.cs
+++ Messager.Customers.Application.Services/DataTransferObjects/CustomerForReadMinimizedDto.cs
@@ -10,5 +10,6 @@ namespace Messager.Customers.Application.Services.DataTransferObjects
     {
         public Guid UserId { get; set; }
         public string UserName { get; set; }
+        public string IconImageSrc { get; set; }
     }
 }
--- Messager.Customers.Application.Services/DataTransferObjects/CustomerForReadPublicInfoDto.cs
+++ Messager.Customers.Application.Services/DataTransferObjects/CustomerForReadPublicInfoDto.cs
@@ -11,5 +11,6 @@ namespace Messager.Customers.Application.Services.DataTransferObjects
         public string LastName { get; set; }
         public string Patronymic { get; set; }
         public DateTime BirthDate { get; set; }
+        public string IconImageSrc { get; set; }
     }
 }
