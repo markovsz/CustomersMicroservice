d97a8860267112a47b03227362729e60dc26999a
Stas <markovszzzz@gmail.com>
2022-08-23 14:08:25 +0300

    customer configuration was moved into new folder

---
+++ Messager.Customers.Infrastructure.Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Messager.Customers.Domain.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Messager.Customers.Infrastructure.Data.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder
+                .HasKey(c => c.UserId);
+        }
+    }
+}
